0cda7b0aedd13b871c8ba2028b7166f223ff269f
shashank-pycharm <shashank.singh1094@gmail.com>
2018-03-17 21:25:36 +0530

    Initial commit

---
+++ 1 helloWorld.py
@@ -0,0 +1,8 @@
+import numpy
+import matplotlib
+import cv2
+
+print("Hello World")
+print("Numpy version", numpy.__version__)
+print("Matplotlib version", matplotlib.__version__)
+print("OpenCV version", cv2.__version__)
