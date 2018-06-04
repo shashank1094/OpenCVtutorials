681fd90f82467181b8d466c9e2f41541afb0d5f4
Shashank Singh <shashank.singh1094@gmail.com>
2018-06-04 12:20:11 +0530

    Added missing modules

--- 1 Hello World.py
+++ 1 Hello World.py
@@ -3,12 +3,18 @@ import matplotlib
 import cv2
 import sys
 import pyzbar
+import imutils
+import skimage
+import sklearn
 
 print("Hello World")
 print("Numpy version", numpy.__version__)
 print("Matplotlib version", matplotlib.__version__)
 print("OpenCV version", cv2.__version__)
 print("PyzBar version", pyzbar.__version__)
+print("imutils version", imutils.__version__)
+print("skimage version", skimage.__version__)
+print("sklearn version", sklearn.__version__)
 print("Python Version", sys.version)
 
 # Hello World
@@ -16,4 +22,7 @@ print("Python Version", sys.version)
 # Matplotlib version 2.2.2
 # OpenCV version 3.4.1
 # PyzBar version 0.1.7
+# imutils version 0.4.6
+# skimage version 0.14.0
+# sklearn version 0.19.1
 # Python Version 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)]
--- README.md
+++ README.md
@@ -8,3 +8,6 @@ OpenCV (Open Source Computer Vision) is a library of programming functions mainl
 4. scikit-image
 5. imutils
 6. pyzbar
+7. imutils
+8. scikit-image
+9. scikit-learn
